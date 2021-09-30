c95e1c194522a4bbef1d348dc9610330a3d090d3
Gautam Prakash <gautamp@reapbenefit.org>
2021-09-30 07:48:02 +0530

    updated text, gsheet address

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
   style: "mapbox://styles/mapbox/light-v10",
   accessToken:
     "pk.eyJ1IjoiZ3RtcHJrc2hyYiIsImEiOiJjamZ0bXBqZnMxd3E5MnduejVjdGpuN2R4In0.vvrRpEdZWNwaKUO6vmgRHw",
-  CSV: "https://docs.google.com/spreadsheets/d/1NWpae4UxuLyMKLVzD-O4QSvgoA1DUbMzcNVE6pIrBmY/gviz/tq?tqx=out:csv&sheet=Bagalkot",
+  CSV: "https://docs.google.com/spreadsheets/d/151qsspW6-Gb58nNTxLJ-oEGYSfPs4PPs6twzgMNLrfI/gviz/tq?tqx=out:csv&sheet=data",
   center: [74.7300352, 31.6334677],
   zoom: 10,
   title: "City Dashboard",
   description:
-    "Everything in your neighbourhood in one place for the city of Bengaluru! Click here to see ward level analytics > https://datastudio.google.com/u/2/reporting/2ae63abd-95d5-47ac-ae95-00c705562a6a/page/9xETC",
+    "Everything in your neighbourhood in one place for the city of Amritsar! Ward level analytics is coming soon",
   sideBarInfo: ["ShopName", "Description"],
   popupInfo: ["ShopName"],
   filters: [
