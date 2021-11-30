5ca72baa4d58ec5285d22f761afe8a06463a94cb
Paul-es <paul.eliassojan@gmail.com>
2021-11-30 22:57:48 +0530

    milestone 1

---
+++ config.js
@@ -0,0 +1,43 @@
+const config = {
+  style: "mapbox://styles/mapbox/light-v10",
+  accessToken:
+    "pk.eyJ1IjoiZ3RtcHJrc2hyYiIsImEiOiJjamZ0bXBqZnMxd3E5MnduejVjdGpuN2R4In0.vvrRpEdZWNwaKUO6vmgRHw",
+  CSV: "https://docs.google.com/spreadsheets/d/1NWpae4UxuLyMKLVzD-O4QSvgoA1DUbMzcNVE6pIrBmY/gviz/tq?tqx=out:csv&sheet=Bagalkot",
+  center: [77.5905634, 12.9244925],
+  zoom: 10,
+  title: "City Dashboard",
+  description:
+    "Everything in your neighbourhood in one place for the city of Bengaluru! See https://datastudio.google.com/u/2/reporting/2ae63abd-95d5-47ac-ae95-00c705562a6a/page/9xETC for ward level analysis",
+  sideBarInfo: ["ShopName", "Description"],
+  popupInfo: ["ShopName"],
+  filters: [
+
+    {
+      type: "dropdown",
+      title: "Category: ",
+      columnHeader: "Category",
+      listItems: [
+        "Govt Services",
+        "Local Data"
+      ],
+    },
+    {
+      type: "dropdown",
+      title: "Sub Category: ",
+      columnHeader: "SubCategory",
+      listItems: [
+        "Others",
+        "Corporators",
+        "Solid Waste",
+        "Citizen Services",
+        "Campaigns",
+        "PHCs",
+        "Volunteers",
+        "Air Quality",
+        "Recharge Well",
+        "Streetlight Intensity",
+        "MalariaHotspot"
+      ],
+    }
+  ],
+}
